c97582bfb9b5622315ca5208fd50d37a3f589d74
yumetTsukiiii <540160575@qq.com>
2020-09-05 11:08:12 +0800

    添加解释器工具

---
+++ src/main/kotlin/AtriScriptInterpreter.kt
@@ -0,0 +1,40 @@
+import cn.yumetsuki.lexical_analysis.DefaultTokenScanner
+import cn.yumetsuki.lexical_analysis.TokenScanner
+import cn.yumetsuki.parser.*
+import cn.yumetsuki.semantic_analysis.ASTVisitor
+import cn.yumetsuki.semantic_analysis.DefaultASTVisitor
+import cn.yumetsuki.semantic_analysis.GlobalMemory
+
+class AtriScriptInterpreter {
+
+    private val tokenScanner: TokenScanner = DefaultTokenScanner()
+
+    private val astParser: ASTParser = ASTParser()
+
+    private val astVisitor: ASTVisitor = DefaultASTVisitor(GlobalMemory())
+
+    fun eval(input: String) : String {
+        val tokens = tokenScanner.scan(input)
+        val nodes = astParser.parse(tokens)
+        return nodes.map {
+            try {
+                when(it) {
+                    is ExpressionNode<*> -> astVisitor.visitExpression(it).toString()
+                    is VariableDefineNode -> astVisitor.visitVariableDefine(it)
+                    is VariableDefineAndAssignmentNode -> astVisitor.visitVariableDefineAndAssignment(it).let {
+                        pair -> "${pair.first} = ${pair.second}"
+                    }
+                    is VariableAssignmentNode -> astVisitor.visitVariableAssignment(it).let {
+                        pair -> "${pair.first} = ${pair.second}"
+                    }
+                    is IdentifierNode -> astVisitor.visitIdentifier(it).toString()
+                    is NumberNode -> astVisitor.visitNumber(it).toString()
+                    else -> "Unsupported syntactic"
+                }
+            } catch (e: Exception) {
+                e.message?:"工口発生"
+            }
+        }.lastOrNull()?:""
+    }
+
+}
--- src/test/kotlin/ScannerTest.kt
+++ src/test/kotlin/ScannerTest.kt
@@ -9,19 +9,10 @@ class ScannerTest {
 
     @Test
     fun testScan() {
-        val scanner: TokenScanner = DefaultTokenScanner()
-        val tokens = scanner.scan("var a = 3;var b = 1+ (3* (a + 4)); 1 + 3 + 4 * 5; a = 5; b = a % 2 + 1")
-        val parser = ASTParser()
-        val nodes = parser.parse(tokens)
-        val visitor = DefaultASTVisitor(GlobalMemory())
-        nodes.forEach {
-            when(it) {
-                is VariableDefineNode -> println(visitor.visitVariableDefine(it))
-                is ExpressionNode<*> -> println(visitor.visitExpression(it))
-                is VariableDefineAndAssignmentNode -> println(visitor.visitVariableDefineAndAssignment(it) )
-                is VariableAssignmentNode -> println(visitor.visitVariableAssignment(it))
-            }
-        }
+        val interpreter = AtriScriptInterpreter()
+        val defineA = "var a = 10\n a + 3"
+        println(interpreter.eval(defineA))
+        println(interpreter.eval("b + 3"))
     }
 
 }
