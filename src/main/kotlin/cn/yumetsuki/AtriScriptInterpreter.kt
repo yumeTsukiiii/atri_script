e2c7072b76a662f3ce8d0ebbac91fe6497a3e4a5
yumetTsukiiii <540160575@qq.com>
2020-09-06 10:32:49 +0800

    修改全局catch异常的位置

--- src/main/kotlin/cn/yumetsuki/AtriScriptInterpreter.kt
+++ src/main/kotlin/cn/yumetsuki/AtriScriptInterpreter.kt
@@ -19,27 +19,27 @@ class AtriScriptInterpreter(
     private val astVisitor: ASTVisitor = DefaultASTVisitor(memory)
 
     fun eval(input: String) : String {
-        val tokens = tokenScanner.scan(input)
-        val nodes = astParser.parse(tokens)
-        return nodes.map {
-            try {
+        return try {
+            val tokens = tokenScanner.scan(input)
+            val nodes = astParser.parse(tokens)
+            return nodes.map {
                 when(it) {
                     is ExpressionNode<*> -> astVisitor.visitExpression(it).toString()
                     is VariableDefineNode -> astVisitor.visitVariableDefine(it)
                     is VariableDefineAndAssignmentNode -> astVisitor.visitVariableDefineAndAssignment(it).let {
-                        pair -> "${pair.first} = ${pair.second}"
+                            pair -> "${pair.first} = ${pair.second}"
                     }
                     is VariableAssignmentNode -> astVisitor.visitVariableAssignment(it).let {
-                        pair -> "${pair.first} = ${pair.second}"
+                            pair -> "${pair.first} = ${pair.second}"
                     }
                     is IdentifierNode -> astVisitor.visitIdentifier(it).toString()
                     is NumberNode -> astVisitor.visitNumber(it).toString()
                     else -> "Unsupported syntactic"
                 }
-            } catch (e: Exception) {
-                e.message?:"工口発生"
-            }
-        }.lastOrNull()?:""
+            }.lastOrNull()?:"工口発生: Unknown error"
+        } catch (e: Exception) {
+            "工口発生: ${e.message}"
+        }
     }
 
 }
